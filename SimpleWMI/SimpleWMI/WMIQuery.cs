032e9e2212ca666609b101bea69ca354813ad420
James Ball <james@jamesball.me>
2018-04-28 11:03:25 +0100

    Created initial function for querying WMI.

--- SimpleWMI/SimpleWMI/WMIQuery.cs
+++ SimpleWMI/SimpleWMI/WMIQuery.cs
@@ -9,5 +9,17 @@ namespace SimpleWMI
 {
     class WMIQuery
     {
+        public static List<dynamic> GetAllObjects(string className)
+        {
+            ManagementObjectCollection col = new ManagementObjectSearcher("SELECT * FROM Win32_" + className).Get();
+            List<dynamic> objects = new List<dynamic>();
+
+            foreach (ManagementObject obj in col)
+            {
+                objects.Add(obj);
+            }
+
+            return objects;
+        }
     }
 }
