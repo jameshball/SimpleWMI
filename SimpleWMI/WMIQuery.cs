c7a872d4623385c41ebfb1aaf49e6be5ad9a799e
James Ball <james@jamesball.me>
2018-04-28 12:38:45 +0100

    Created fundemental functions for easily querying WMI classes and examples of using this.

---
+++ SimpleWMI/WMIQuery.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Dynamic;
+
+namespace SimpleWMI
+{
+    public class WMIQuery
+    {
+        public static List<dynamic> GetAllObjects(string className)
+        {
+            ManagementObjectCollection col = new ManagementObjectSearcher("SELECT * FROM Win32_" + className).Get();
+            List<dynamic> objects = new List<dynamic>();
+
+            foreach (ManagementObject obj in col)
+            {
+                var currentObject = new ExpandoObject() as IDictionary<string, Object>;
+
+                foreach (PropertyData prop in obj.Properties)
+                {
+                    currentObject.Add(prop.Name, prop.Value);
+                }
+
+                objects.Add(currentObject);
+            }
+
+            return objects;
+        }
+
+        public static dynamic GetObject(string className)
+        {
+            ManagementObjectCollection col = new ManagementObjectSearcher("SELECT * FROM Win32_" + className).Get();
+
+            var currentObject = new ExpandoObject() as IDictionary<string, Object>;
+
+            //There will only be one value returned (not a list), but ManagementObjectCollection does not allow indexing.
+            foreach (ManagementObject obj in col)
+            {
+                foreach (PropertyData prop in obj.Properties)
+                {
+                    currentObject.Add(prop.Name, prop.Value);
+                }
+
+                break;
+            }
+
+            return currentObject;
+        }
+    }
+}
---
+++ SimpleWMIExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using SimpleWMI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWMIExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<dynamic> googleProcesses = WMIQuery.GetAllObjects("Process").Where(s => s.Caption.ToLower().Contains("google"));
+
+            foreach (dynamic item in googleProcesses)
+            {
+                Console.WriteLine(item.Caption);
+            }
+
+            //IEnumerable<object> results = processes.Where(s => s.Caption.ToLower().Contains("google"));
+
+            //foreach (dynamic item in results)
+            //{
+            //    Console.WriteLine(item.Caption);
+            //}
+
+            Console.Read();
+        }
+    }
+}
