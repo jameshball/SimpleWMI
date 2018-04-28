7d9e1a98ae868505f4cc1bb9ceb5f4c83db8c118
James Ball <james@jamesball.me>
2018-04-28 10:41:10 +0100

    Created initial Visual Studio Project.

---
+++ SimpleWMI/SimpleWMI/WMIQuery.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Management;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWMI
+{
+    class WMIQuery
+    {
+    }
+}
