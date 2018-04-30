ae4109fd48804f63cfff6c8e5311c56339b3384e
James Ball <james@jamesball.me>
2018-04-30 19:47:03 +0100

    Added more examples of applications of the library.

--- SimpleWMIExample/Program.cs
+++ SimpleWMIExample/Program.cs
@@ -11,6 +11,8 @@ namespace SimpleWMIExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Processes currently running with 'google' in the caption:");
+
             IEnumerable<dynamic> googleProcesses = WMIQuery.GetAllObjects(Win32.Process).Where(s => s.Caption.ToLower().Contains("google"));
 
             foreach (dynamic item in googleProcesses)
@@ -18,9 +20,9 @@ namespace SimpleWMIExample
                 Console.WriteLine(item.Caption);
             }
 
-            dynamic cpu = WMIQuery.GetObject("Win32_Processor");
-            Console.WriteLine(cpu.Name);
+            Console.WriteLine();
 
+            Console.WriteLine("Names of detected GPUs in this computer:");
             foreach (dynamic item in WMIQuery.GetAllObjects(Win32.VideoController))
             {
                 //Wrapped in a try-catch because trying to write null values to console causes errors.
@@ -32,6 +34,47 @@ namespace SimpleWMIExample
                 {
                     Console.WriteLine(ex.Message);
                 }
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Information about connected monitors:");
+
+            foreach (dynamic item in WMIQuery.GetAllObjects(Win32.DesktopMonitor))
+            {
+                Console.WriteLine("Resolution: {0}x{1}", item.ScreenWidth, item.ScreenHeight);
+                Console.WriteLine("Manufacturer: {0}", item.MonitorManufacturer);
+                Console.WriteLine("Name: {0}", item.Name);
+                Console.WriteLine("Device ID: {0}", item.DeviceID);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Information about connected printers:");
+
+            foreach (dynamic item in WMIQuery.GetAllObjects(Win32.Printer))
+            {
+                Console.WriteLine("Name: {0}", item.Name);
+                Console.WriteLine("Device ID: {0}", item.DeviceID);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Information about connected mice:");
+
+            foreach (dynamic item in WMIQuery.GetAllObjects(Win32.PointingDevice))
+            {
+                Console.WriteLine("Name: {0}", item.Name);
+                Console.WriteLine("Manufacturer: {0}", item.Manufacturer);
+                Console.WriteLine("Device ID: {0}", item.DeviceID);
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Information about connected keyboards:");
+
+            foreach (dynamic item in WMIQuery.GetAllObjects(Win32.Keyboard))
+            {
+                Console.WriteLine("Name: {0}", item.Name);
+                Console.WriteLine("Device ID: {0}", item.DeviceID);
+                Console.WriteLine();
             }
 
             Console.Read();
